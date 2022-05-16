db39f60a7975fc27d37290a0508576ed6c682a41
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-05-16 21:08:45 +0530

    some errors were fixed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-const users = require('./routes/users');
-const express = require('express')
+require("dotenv").config();
+const config = require("config");
+const users = require("./routes/users");
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 app.use(express.json());
-app.use('/texas-e-store/api/users', users);
+app.use("/texas-e-store/api/users", users);
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
