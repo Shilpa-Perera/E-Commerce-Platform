6a02fc752b3ad48b65b6c1d7d33af8f234c49ccf
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-21 16:59:36 +0530

    Separated DDL and DML

--- server/ddl-texas-e-store.sql
+++ server/ddl-texas-e-store.sql
@@ -9,27 +9,3 @@ create table if not exists users (
     password VARCHAR(1024) NOT NULL,
     gender VARCHAR(8) NOT NULL
 );
-
-INSERT INTO users (name, email, password, gender)
-VALUES (
-    'Chaturanga Jayanath',
-    'chathuranga@gmail.com',
-    '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka',
-    'male'
-  );
-
-INSERT INTO users (name, email, password, gender)
-VALUES (
-    'Nipuni Silva',
-    'nipuni@gmail.com',
-    '$2b$10$XJTjhQfCLlg4sCrakL.ih.DaeXTb7SjrltITr/x4WgscUubBDDMRq',
-    'female'
-  );
-
-INSERT INTO users (name, email, password, gender)
-VALUES (
-    'Kumesh Uminda',
-    'kumesh@gmail.com',
-    '$2b$10$TtrjJrQQn0LkvNJc3wUxLeWmWgji3b23Jq4rgIbzDJ39VauVTR5Zy',
-    'male'
-  );
