fda0dfabed037455689463e74127893daf5b24bd
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-21 17:42:56 +0530

    Added DDL for product, variant, variant_option, custom_feature tables

--- server/ddl-texas-e-store.sql
+++ server/ddl-texas-e-store.sql
@@ -9,3 +9,37 @@ create table if not exists users (
     password VARCHAR(1024) NOT NULL,
     gender VARCHAR(8) NOT NULL
 );
+
+drop table if exists product;
+create table if not exists product (
+    product_id int unsigned auto_increment primary key,
+    product_title varchar(255) not null,
+    sku varchar(32) not null,
+    product_weight float not null
+);
+
+drop table if exists variant;
+create table if not exists variant (
+    variant_id int unsigned auto_increment primary key,
+    product_id int unsigned not null,
+    variant_name varchar(255) not null,
+    price decimal(15, 2) not null,
+    quantity int not null,
+    foreign key (product_id) references product(product_id)
+);
+
+drop table if exists variant_option;
+create table if not exists variant_option (
+    option_id int unsigned auto_increment primary key,
+    product_id int unsigned not null,
+    variant_name varchar(255),
+    foreign key (product_id) references product(product_id)
+);
+
+drop table if exists custom_feature;
+create table if not exists custom_feature (
+    custom_feature_id int unsigned auto_increment primary key,
+    product_id int unsigned not null,
+    custom_feature_name varchar(255) not null,
+    custom_feature_val varchar(255) not null
+);
