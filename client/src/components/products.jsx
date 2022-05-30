c9fa15e85412d024afc2552726aec7ac148bd2ed
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-30 07:29:56 +0530

    Fixed compatibility issues with subcategory

--- client/src/components/products.jsx
+++ client/src/components/products.jsx
@@ -6,71 +6,75 @@ import CategoryList from "./categoryList";
 import SubCategoryList from "./subCategoryList";
 
 class Products extends Component {
-  state = {
-    products: [],
-    categories: [],
-    subCategories: [],
-    selectedCategory: null,
-    selectedSubCategory: null,
-  };
+    state = {
+        products: [],
+        categories: [],
+        subCategories: [],
+        selectedCategory: null,
+        selectedSubCategory: null,
+    };
 
-  async componentDidMount() {
-    const { data: products } = await getProducts();
-    const { data: categories } = await getCategories();
-    this.setState({ products, categories });
-  }
+    async componentDidMount() {
+        const { data: products } = await getProducts();
+        const { data: categories } = await getCategories();
+        this.setState({ products, categories });
+    }
 
-  handleCategorySelect = (category) => {
-    const { data: subCategories } = getSubCategories(category.category_id);
-    this.setState({
-      selectedCategory: category,
-      subCategories: subCategories,
-      selectedSubCategory: null,
-      // currentPage: 1,
-      // searchQuery: "",
-    });
-  };
+    handleCategorySelect = async (category) => {
+        const { data: subCategories } = await getSubCategories(
+            category.category_id
+        );
+        this.setState({
+            selectedCategory: category,
+            subCategories: subCategories,
+            selectedSubCategory: null,
+            // currentPage: 1,
+            // searchQuery: "",
+        });
+    };
 
-  handleSubCategorySelect = (subCategory) => {
-    this.setState({
-      selectedSubCategory: subCategory,
-      // currentPage: 1,
-      // searchQuery: "",
-    });
-  };
+    handleSubCategorySelect = (subCategory) => {
+        this.setState({
+            selectedSubCategory: subCategory,
+            // currentPage: 1,
+            // searchQuery: "",
+        });
+    };
 
-  render() {
-    const {
-      products,
-      categories,
-      selectedCategory,
-      subCategories,
-      selectedSubCategory,
-    } = this.state;
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-4 col-lg-3 col-xxl-2">
-            <CategoryList
-              categories={categories}
-              handleCategorySelect={this.handleCategorySelect}
-              selectedCategory={selectedCategory}
-            />
-          </div>
-          <div className="col-md-8 col-lg-9 col-xxl-10">
-            {selectedCategory && (
-              <SubCategoryList
-                subCategories={subCategories}
-                handleSubCategorySelect={this.handleSubCategorySelect}
-                selectedSubCategory={selectedSubCategory}
-              />
-            )}
-            <ProductAlbum products={products} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {
+            products,
+            categories,
+            selectedCategory,
+            subCategories,
+            selectedSubCategory,
+        } = this.state;
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-4 col-lg-3 col-xxl-2">
+                        <CategoryList
+                            categories={categories}
+                            handleCategorySelect={this.handleCategorySelect}
+                            selectedCategory={selectedCategory}
+                        />
+                    </div>
+                    <div className="col-md-8 col-lg-9 col-xxl-10">
+                        {selectedCategory && (
+                            <SubCategoryList
+                                subCategories={subCategories}
+                                handleSubCategorySelect={
+                                    this.handleSubCategorySelect
+                                }
+                                selectedSubCategory={selectedSubCategory}
+                            />
+                        )}
+                        <ProductAlbum products={products} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Products;
--- client/src/services/categoryService.js
+++ client/src/services/categoryService.js
@@ -3,31 +3,14 @@ import http from "./httpService";
 const apiUrl = process.env.REACT_APP_API_URL;
 const apiEndpoint = apiUrl + "/categories";
 
-export function getCategories() {
-  return http.get(apiEndpoint);
+function categoryUrl(id) {
+    return `${apiEndpoint}/${id}`;
 }
 
-const categories = [
-  { category_id: 1, category_name: "Mobile Phones" },
-  { category_id: 2, category_name: "Tablets" },
-  { category_id: 3, category_name: "Laptops" },
-  { category_id: 4, category_name: "Desktops" },
-  { category_id: 5, category_name: "External Storage" },
-  { category_id: 6, category_name: "Monitors" },
-  { category_id: 7, category_name: "KeyBoards and Mice" },
-  { category_id: 8, category_name: "Smart Watches" },
-  { category_id: 9, category_name: "Smart TVs" },
-  { category_id: 10, category_name: "Routers" },
-];
-
-const subCategories = [
-  { subcategory_id: 1, subcategory_name: "Apple" },
-  { subcategory_id: 2, subcategory_name: "Google" },
-  { subcategory_id: 3, subcategory_name: "Microsoft" },
-  { subcategory_id: 4, subcategory_name: "Nokia" },
-  { subcategory_id: 5, subcategory_name: "Samsung" },
-];
+export function getCategories() {
+    return http.get(apiEndpoint);
+}
 
 export function getSubCategories(category_id) {
-  return { data: subCategories };
+    return http.get(categoryUrl(category_id));
 }
