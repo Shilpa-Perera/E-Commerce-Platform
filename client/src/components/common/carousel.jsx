c68c44b4292162fbd6f16fa3aa95882e4af41bc4
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-05-31 11:16:18 +0530

    Added carousel component

---
+++ client/src/components/common/carousel.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+function Carousel({ id, images }) {
+    return (
+        <div>
+            <div
+                id={id}
+                className="carousel slide"
+                data-bs-ride="carousel"
+            >
+                <div className="carousel-indicators">
+                    {images.map((image, index) => (
+                        <button
+                            type="button"
+                            data-bs-target={`#${id}`}
+                            data-bs-slide-to={index}
+                            aria-current={index === 0}
+                            aria-label={`Slide ${index}`}
+                            className={index === 0 ? "active" : ""}
+                            key={index}
+                        ></button>
+                    ))}
+                </div>
+                <div className="carousel-inner">
+                    {images.map((image, index) => (
+                        <div
+                            className={
+                                index === 0
+                                    ? "carousel-item active"
+                                    : "carousel-item"
+                            }
+                            key={index}
+                        >
+                            <img
+                                src={image}
+                                className="d-block w-100"
+                                alt="..."
+                            />
+                        </div>
+                    ))}
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target={`#${id}`}
+                    data-bs-slide="prev"
+                >
+                    <span
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target={`#${id}`}
+                    data-bs-slide="next"
+                >
+                    <span
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Carousel;
