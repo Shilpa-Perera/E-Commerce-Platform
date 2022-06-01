c1fcc86393961e9fbb8efe2a888d8f29a1231168
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-06-01 16:08:33 +0530

    Added put method to update a variant

--- client/src/services/variantService.js
+++ client/src/services/variantService.js
@@ -1,4 +1,5 @@
 import http from "./httpService";
+import _ from "lodash";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 const apiEndpoint = apiUrl + "/variants";
@@ -10,4 +11,11 @@ function variantUrl(id) {
 export function getVariant(product_id, options) {
     const body = JSON.stringify({ product_id, options });
     return http.get(apiEndpoint, { params: { body } });
+}
+
+export function updateVariant(variant) {
+    return http.put(
+        variantUrl(variant.variant_id),
+        _.pick(variant, ["variant_name", "price", "quantity"])
+    );
 }
