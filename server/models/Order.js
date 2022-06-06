d5ad3c64f99632fb36a057138c787e5d6c15e684
Achira Dias <achira828@gmail.com>
2022-06-06 15:24:39 +0530

    Update Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -4,12 +4,27 @@ dotenv.config();
 
 class Order{
     constructor(orderDetails) {
-        this.order_Id = orderDetails.order_Id;
+        this.order_id = orderDetails.order_id;
+        this.customer_id = orderDetails.customer_id;
+        this.cart_id = orderDetails.cart_id;
+        this.date = orderDetails.date;
+        this.order_name = orderDetails.order_name;
+        this.delivery_address = orderDetails.delivery_address;
+        this.phone_number = orderDetails.phone_number;
+        this.delivery_method = orderDetails.delivery_method;
+        this.payment = orderDetails.payment;
         
     }
 
     static fetchAll() {
-        return db.execute('select * from order;');
+        const select_all_query = 'select * from order;'
+        return db.execute(select_all_query);
+    }
+
+    static async getOrderById(orderId){
+        const product_values_query = "select * from order where order_id = ?";
+        const result = await db.execute(product_values_query, [orderId]); 
+        return result[0];
     }
     
 }
