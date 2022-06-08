2484e5b4dd8abbb2df8743d2bbef1847a8c16c12
Achira Dias <achira828@gmail.com>
2022-06-08 16:26:27 +0530

    Update order.js

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -3,11 +3,10 @@ const { Order } = require("../models/Order");
 
 class OrderController {
     static async getAllOrders(req, res, next) {
-        const allOrders = await Order.fechAll();
+        const allOrders = await Order.fetchAll();
+        console.log("api called orders sent...");
         res.send(allOrders[0]);
     }
-
-   
 }
 
-module.exports.ProductController = ProductController;
+module.exports.OrderController = OrderController;
