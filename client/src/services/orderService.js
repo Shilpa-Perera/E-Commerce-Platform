3c120326f7cb01dc626c7442c3afab8821ca25d1
Achira Dias <achira828@gmail.com>
2022-06-09 01:24:33 +0530

    Create orderService.js

---
+++ client/src/services/orderService.js
@@ -0,0 +1,10 @@
+import http from "./httpService";
+import _ from "lodash";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiEndpoint = apiUrl + "/orders";
+
+export function getOrders() {
+    console.log(apiEndpoint);
+    return http.get(apiEndpoint);
+}
