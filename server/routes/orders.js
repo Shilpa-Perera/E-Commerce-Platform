bc1b984017609b437ebee107050012941af79320
Achira Dias <achira828@gmail.com>
2022-06-08 16:26:11 +0530

    Update orders.js

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,10 +1,10 @@
-const { OrderController: OrderController } = require("../controllers/order");
+const { OrderController } = require("../controllers/order");
 const express = require("express");
 const router = express();
 
 router.get("/", OrderController.getAllOrders);
-router.get("/:id", OrderController.getOrder);
-router.post("/", OrderController.postOrder);
-router.put("/:id", OrderController.putOrder);
+// router.get("/:id", OrderController.getOrder);
+// router.post("/", OrderController.postOrder);
+// router.put("/:id", OrderController.putOrder);
 
 module.exports = router;
