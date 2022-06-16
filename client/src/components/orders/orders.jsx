3ac04b7d3df118e6662e29ed1457dc0970b77766
Achira Dias <achira828@gmail.com>
2022-06-16 23:47:33 +0530

    Update orders.jsx

--- client/src/components/orders/orders.jsx
+++ client/src/components/orders/orders.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import _ from "lodash";
-import { Link } from "react-router-dom";
 import { getOrders } from "../../services/orderService";
 import { paginate } from "../../utils/paginate";
 import OrdersTable from "./ordersTable";
