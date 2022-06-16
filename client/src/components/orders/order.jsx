f2c2a3aa4f63b13dd54b7fcf4e1e381ea155668e
Achira Dias <achira828@gmail.com>
2022-06-16 23:47:29 +0530

    Update order.jsx

--- client/src/components/orders/order.jsx
+++ client/src/components/orders/order.jsx
@@ -1,19 +1,29 @@
-const { Component } = require("react");
+import React, { Component } from "react";
+import {useParams} from "react-router-dom";
 
 
-class Order extends Component{
+class OrderView extends Component{
     state = {
         order: null,
         cartId: null
     }
+
+    // async componentDidMount(){
+    //     const { id } = this.props;
+    // }
     
     render(){
         const {id} = this.props;
         console.log(id);
         return (
-            <div>sssss </div>
+            <div>Order Id is :  {id} </div>
         );
     }
+}
+
+function Order(props) {
+    const { id } = useParams();
+    return <OrderView {...{ props, id}} />;
 }
 
 export default Order
