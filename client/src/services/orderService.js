a7d95f72141052836fdc4f9d7693b314e1933456
Achira Dias <achira828@gmail.com>
2022-06-16 23:53:36 +0530

    Remove unnecessary console logs

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -5,6 +5,5 @@ const apiUrl = process.env.REACT_APP_API_URL;
 const apiEndpoint = apiUrl + "/orders";
 
 export function getOrders() {
-    console.log(apiEndpoint);
     return http.get(apiEndpoint);
 }
--- server/controllers/order.js
+++ server/controllers/order.js
@@ -4,7 +4,6 @@ const { Order } = require("../models/Order");
 class OrderController {
     static async getAllOrders(req, res, next) {
         const allOrders = await Order.fetchAll();
-        console.log("api called orders sent...");
         res.send(allOrders[0]);
     }
 }
