55908ded6a276c98cec4cd207faaa17788132dc1
Achira Dias <80286616+Invader-hub@users.noreply.github.com>
2022-06-17 16:41:36 +0530

    Update order.jsx

--- client/src/components/orders/order.jsx
+++ client/src/components/orders/order.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { useParams } from "react-router-dom";
+import { getOrder } from "../../services/orderService";
 import OrdersCartTable from "./orderViewCartTable";
 
 class OrderView extends Component {
@@ -9,124 +10,127 @@ class OrderView extends Component {
         cart: []
     };
 
-    async componentDidMount(){
-        const { data: cart } = 4;
-        this.setState({cart});
+    async componentDidMount() {
+        const { id } = this.props;
+        const { data: cart } = await getOrder(id);
+
+        this.setState({ cart });
     }
 
     render() {
         const { id } = this.props;
-        const {cart} = this.state;
+        const { cart } = this.state;
         console.log(id);
+        console.log(cart);
         return (
             <div className="pb-5">
                 <div className="container div-dark">
                     <h3 className="mb-4">Manage Order ### WIP</h3>
                     <div className="mt-5">
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Order ID
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value={id}
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Name on order
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="Gotabaya Rajapaksa"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Date
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="2022/02/25"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Delivery Address
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="No. 69, Pohottu rd, Mirihana"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Telephone
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="01169696969"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Delivery Method
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="Tank"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3 row">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3 row">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Payment Method
                             </label>
-                            <div class="col-sm-6">
+                            <div className="col-sm-6">
                                 <input
                                     type="text"
                                     readonly
-                                    class="form-control-plaintext"
+                                    className="form-control-plaintext"
                                     id="f1"
                                     value="Cash"
                                 />
                             </div>
                         </div>
-                        <div class="mb-3">
-                            <label for="f1" class="col-sm-4 col-form-label">
+                        <div className="mb-3">
+                            <label for="f1" className="col-sm-4 col-form-label">
                                 Cart {cart}
                             </label>
                             {/* <OrdersCartTable /> */}
-                            <div class="col-sm-6"></div>
+                            <div className="col-sm-6"></div>
                         </div>
                     </div>
                 </div>
