c907ef267d1daa9d65632b2da76e205d65dcbffd
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-06-17 12:05:08 +0530

    Reorganized routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -20,9 +20,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-router.get("/", ProductController.getAllProducts);
 router.get("/unavailable", ProductController.getUnavailableProducts);
 router.get("/:id", ProductController.getProduct);
+router.get("/", ProductController.getAllProducts);
 router.post("/feature", ProductController.postCustomFeature);
 router.post("/", ProductController.postProduct);
 router.put("/feature/:id", ProductController.putCustomFeature);
