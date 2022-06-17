d569bed9601f0824dcfc4c7bd25115b0a8b3447f
Achira Dias <80286616+Invader-hub@users.noreply.github.com>
2022-06-17 16:41:09 +0530

    Update order.js

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -7,6 +7,12 @@ class OrderController {
         console.log("api called orders sent...");
         res.send(allOrders[0]);
     }
+
+    static async getOrderCart(req, res, next){
+        const {id} = req.params;
+        const orderCart = await Order.getOrderCart(id);
+        res.send(orderCart[0]);
+    }
 }
 
 module.exports.OrderController = OrderController;
