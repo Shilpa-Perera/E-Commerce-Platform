b37da43869836134c72ddde0e917c0b46fe81349
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-06-17 09:45:43 +0530

    Changed cart title color

--- client/src/components/cart.jsx
+++ client/src/components/cart.jsx
@@ -57,7 +57,7 @@ class Cart extends Component {
         <div className="col-10">
 
           <div className="d-flex justify-content-between align-items-center mb-4">
-            <h3 className="fw-normal mb-0 text-white">Shopping Cart</h3>
+            <h3 className="fw-normal mb-0 text-invert">Shopping Cart</h3>
               <div>
                 <p className="mb-0"><span className="text-muted">Sort by:</span> <a href="#!" className="text-body">price <i
                   className="fa fa-angle-down mt-1"></i></a></p>
