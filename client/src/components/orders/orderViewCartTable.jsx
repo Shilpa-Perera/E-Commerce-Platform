5d221a6b5da25d2bc1ae80b9388fec3ffa962257
Achira Dias <achira828@gmail.com>
2022-06-17 20:04:06 +0530

    Update orderViewCartTable.jsx

--- client/src/components/orders/orderViewCartTable.jsx
+++ client/src/components/orders/orderViewCartTable.jsx
@@ -3,57 +3,28 @@ import { Link } from "react-router-dom";
 import Table from "../common/table";
 
 class OrdersCartTable extends Component {
-    columns = [
-        {
-            key: "payment_method",
-            content: ({ payment_method }) => (
-                <span className="me-2 my-2 my-lg-0">{payment_method}</span>
-            ),
-            label: "payment method",
-        },
-        {
-            key: "delivery_method",
-            content: ({ delivery_method }) => (
-                <span className="me-2 my-2 my-lg-0">{delivery_method}</span>
-            ),
-            label: "Delivery Method",
-        },
-        {
-            key: "phone_number",
-            content: ({ phone_number }) => (
-                <span className="me-2 my-2 my-lg-0">{phone_number}</span>
-            ),
-            label: "Phone number",
-        },
-        {
-            key: "order_idK",
-            content: ({ order_id }) => (
-                <div className="d-flex flex-column flex-lg-row mb-3">
-                    <span className="me-2 my-2 my-lg-0">
-                        <button className="btn btn-success btn-sm hover-focus">
-                            <span className="me-2">View Order</span>
-                            <i className="fa fa-eye"></i>
-                        </button>
-                    </span>
-                </div>
-            ),
-            label: "Action",
-        },
-    ];
+    columns = [];
+    
 
     render() {
-        const { orders, sortBy, onSort } = this.props;
+        const { orders, sortBy, onSort, cartDetails } = this.props;
+        console.log(cartDetails);
         return (
             <div className="pb-5">
                 <div className="container div-dark">
                     <div className="mt-5">
                         <div className="table-responsive order-table-container">
-                            <Table
+                            {cartDetails.map((e) => {
+                                let name = e.variant_name;
+                                console.log(name);
+                                return (<div key={name}>{name}</div>);
+                            })}
+                            {/* <Table
                                 columns={this.columns}
                                 data={orders}
                                 sortColumn={sortBy}
                                 onSort={onSort}
-                            />
+                            /> */}
                         </div>
                     </div>
                 </div>
