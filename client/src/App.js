61e3a9fd78bd80b4445becc6413d628455060e81
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-06-18 22:04:43 +0530

    Restored old 'App.js'

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import Products from "./components/products/products";
 import NavBar from "./components/navBar";
 import "react-toastify/dist/ReactToastify.css";
 import ProductForm from "./components/products/productForm";
-// import ProductForm from "./components/productForm";
-import CategoryForm from "./components/categoryForm";
 import Footer from "./components/footer";
 import Product from "./components/products/product";
 import VariantForm from "./components/variants/variantForm";
@@ -23,123 +21,27 @@ import ScrollToTop from "./components/scrollToTop";
 import DeletedProduct from "./components/products/deletedProduct";
 import ThemeSelector from "./components/themeSelector";
 import UnavailableProducts from "./components/products/unavailableProducts";
-<<<<<<< HEAD
-import { getTheme, saveTheme } from "./services/themeService";
+import { getTheme, saveTheme } from "./utils/theme";
+import { getCurrentUser } from "./services/authService";
+import ProtectedRoute from "./components/common/protectedRoute";
 import {
-  getItemCount,
-  addProductToCart,
-  setCartId,
-  incrementItemCount,
-  decrementItemCount,
-  deletedProduct,
+    getItemCount,
+    addProductToCart,
+    setCartId,
+    incrementItemCount,
+    decrementItemCount,
+    deletedProduct,
 } from "./services/cartService";
 import { toast } from "react-toastify";
 
 function App() {
-  const [theme, setTheme] = useState(getTheme());
-  const [item_count, setItemCount] = useState(getItemCount());
-
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <ThemeSelector theme={theme} />
-      <NavBar
-        theme={theme}
-        toggleTheme={(theme) => {
-          setTheme(theme);
-          saveTheme(theme);
-        }}
-        item_count={item_count}
-      />
-      <ToastContainer />
-      <Routes>
-        <Route path="/products">
-          <Route
-            index
-            element={<Products isAlbum={true} isTable={false} />}
-          ></Route>
-
-          <Route
-            path="new"
-            element={<Navigate to="/products/edit/new" />}
-          ></Route>
-
-          <Route path="unavailable" element={<UnavailableProducts />}></Route>
-
-          <Route path="deleted" element={<DeletedProduct />}></Route>
-
-          <Route path=":id">
-            <Route
-              index
-              element={
-                <Product
-                  item_count={item_count}
-                  onAddToCart={async (variant_id) => {
-                    await setCartId();
-                    incrementItemCount();
-
-                    const cart_id = localStorage.getItem("cart_id");
-                    const obj = {
-                      cart_id: cart_id,
-                      variant_id: variant_id,
-                    };
-                    const { data: result } = await addProductToCart(obj);
-                    toast.success(`Item added to cart!`, { theme: "dark" });
-                    setItemCount(getItemCount());
-                  }}
-                />
-              }
-            ></Route>
-
-            <Route path="variants">
-              <Route index element={<VariantForm />}></Route>
-
-              <Route path=":v_id">
-                <Route path="images" element={<VariantImages />}></Route>
-              </Route>
-            </Route>
-          </Route>
-
-          <Route path="edit">
-            <Route
-              index
-              element={<Products isAlbum={false} isTable={true} />}
-            ></Route>
-
-            <Route path=":id" element={<ProductForm />}></Route>
-          </Route>
-        </Route>
-
-        <Route path="/orders">
-          <Route index element={<Orders />}></Route>
-          <Route path=":id" element={<Order />}></Route>
-        </Route>
-
-        <Route
-          path="/cart"
-          element={
-            <Cart
-              item_count={item_count}
-              onDeleteFromCart={async (cart_id, variant_id) => {
-                decrementItemCount();
-                await deletedProduct(cart_id, variant_id);
-                setItemCount(getItemCount);
-              }}
-=======
-import { getTheme, saveTheme } from "./utils/theme";
-import { getCurrentUser } from "./services/authService";
-import ProtectedRoute from './components/common/protectedRoute';
-import {  getItemCount ,addProductToCart , setCartId, incrementItemCount, decrementItemCount ,deletedProduct } from "./services/cartService";
-import { toast } from "react-toastify";
-
-function App() {
     const [theme, setTheme] = useState(getTheme());
-    const [item_count , setItemCount] = useState(getItemCount()) ;
+    const [item_count, setItemCount] = useState(getItemCount());
     const [user, setUser] = useState();
 
     useEffect(() => {
         setUser(getCurrentUser());
-    }, [])
-
+    }, []);
 
     return (
         <div className="d-flex flex-column min-vh-100">
@@ -151,50 +53,54 @@ function App() {
                     setTheme(theme);
                     saveTheme(theme);
                 }}
-                item_count = {item_count}
->>>>>>> 83ca9290c69cdbf617ec721287562846bb6dbbe1
+                item_count={item_count}
             />
-          }
-        ></Route>
+            <ToastContainer />
+            <Routes>
+                <Route path="/products">
+                    <Route
+                        index
+                        element={<Products isAlbum={true} isTable={false} />}
+                    ></Route>
 
-        <Route path="/login" element={<LoginForm />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
+                    <Route
+                        path="new"
+                        element={<Navigate to="/products/edit/new" />}
+                    ></Route>
 
-        <Route path="/customers/*">
-          <Route path="register" element={<RegisterForm />}></Route>
-        </Route>
+                    <Route
+                        path="unavailable"
+                        element={<UnavailableProducts />}
+                    ></Route>
 
-        <Route path="/" element={<Home />}></Route>
+                    <Route path="deleted" element={<DeletedProduct />}></Route>
 
-<<<<<<< HEAD
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <ScrollToTop />
-      <Footer />
-    </div>
-  );
-=======
                     <Route path=":id">
-                        <Route index element={<Product item_count = {item_count} 
-                                                       onAddToCart={async (variant_id)  => {
-                                                           
+                        <Route
+                            index
+                            element={
+                                <Product
+                                    item_count={item_count}
+                                    onAddToCart={async (variant_id) => {
+                                        await setCartId();
+                                        incrementItemCount();
 
-                                                            await setCartId();
-                                                            incrementItemCount();
-                                                            
-                                                    
-                                                            const cart_id = localStorage.getItem("cart_id");
-                                                            const obj = {
-                                                                cart_id: cart_id,
-                                                                variant_id: variant_id,
-                                                            };
-                                                            const { data: result } = await addProductToCart(obj);
-                                                            toast.success(`Item added to cart!`, { theme: "dark" });
-                                                            setItemCount(getItemCount());
-                                                }} 
-                            />}>
-
-                     </Route>
+                                        const cart_id =
+                                            localStorage.getItem("cart_id");
+                                        const obj = {
+                                            cart_id: cart_id,
+                                            variant_id: variant_id,
+                                        };
+                                        const { data: result } =
+                                            await addProductToCart(obj);
+                                        toast.success(`Item added to cart!`, {
+                                            theme: "dark",
+                                        });
+                                        setItemCount(getItemCount());
+                                    }}
+                                />
+                            }
+                        ></Route>
 
                         <Route path="variants">
                             <Route index element={<VariantForm />}></Route>
@@ -225,27 +131,54 @@ function App() {
                     <Route path=":id" element={<Order />}></Route>
                 </Route>
 
-                <Route path="/cart" element={<Cart item_count = {item_count} 
-                                                    onDeleteFromCart = {async (cart_id,variant_id)=>{
-                                                         decrementItemCount()
-                                                         await deletedProduct(cart_id ,variant_id) ;
-                                                         setItemCount(getItemCount);
-                                                }}/>}>
+                <Route
+                    path="/cart"
+                    element={
+                        <Cart
+                            item_count={item_count}
+                            onDeleteFromCart={async (cart_id, variant_id) => {
+                                decrementItemCount();
+                                await deletedProduct(cart_id, variant_id);
+                                setItemCount(getItemCount);
+                            }}
+                        />
+                    }
+                ></Route>
 
-                                                </Route>
-
-                <Route path="/login" element={<LoginForm setUser={(user) => {setUser(user);}} />}></Route>
-                <Route path="/logout" element={<Logout setUser={(user) => {setUser(user);}}/>}></Route>
+                <Route
+                    path="/login"
+                    element={
+                        <LoginForm
+                            setUser={(user) => {
+                                setUser(user);
+                            }}
+                        />
+                    }
+                ></Route>
+                <Route
+                    path="/logout"
+                    element={
+                        <Logout
+                            setUser={(user) => {
+                                setUser(user);
+                            }}
+                        />
+                    }
+                ></Route>
 
                 <Route path="/customers/*">
-                    
-                        <Route path=":id" element={
-                            <ProtectedRoute><CustomerForm /></ProtectedRoute>
-                        }></Route>
+                    <Route
+                        path=":id"
+                        element={
+                            <ProtectedRoute>
+                                <CustomerForm />
+                            </ProtectedRoute>
+                        }
+                    ></Route>
                     {/* <Route path=":id" element={
                             <CustomerForm />
                         }></Route> */}
-                    
+
                     <Route path="register" element={<CustomerForm />}></Route>
                 </Route>
 
@@ -257,7 +190,6 @@ function App() {
             <Footer />
         </div>
     );
->>>>>>> 83ca9290c69cdbf617ec721287562846bb6dbbe1
 }
 
 export default App;
