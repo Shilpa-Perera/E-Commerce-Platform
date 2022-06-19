752d90e6b74e36315c26fea889be173981cf88be
Achira Dias <achira828@gmail.com>
2022-06-20 03:45:58 +0530

    Update checkoutFormCard.jsx

--- client/src/components/orders/checkoutFormCard.jsx
+++ client/src/components/orders/checkoutFormCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 
 class CheckoutFormCard extends Component {
     state = [{ payment: null }];
@@ -74,39 +74,15 @@ class CheckoutFormCard extends Component {
                     ></input>
                 </div>
 
-                <h5 className="mb-3">Payment method</h5>
+                <Link to={`/cart/checkout/payment`}>
+                    <button
+                        className="btn btn-primary btn-lg btn-block"
+                        type="submit"
+                    >
+                        Proceed to Payment
+                    </button>
+                </Link>
 
-                <div className="d-block my-3">
-                    <div class="form-check">
-                        <input
-                            class="form-check-input"
-                            type="radio"
-                            name="flexRadioDefault"
-                            id="cashInput"
-                        ></input>
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            Cash
-                        </label>
-                    </div>
-
-                    <div class="form-check">
-                        <input
-                            class="form-check-input"
-                            type="radio"
-                            name="flexRadioDefault"
-                            id="cardInput"
-                        ></input>
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            Card
-                        </label>
-                    </div>
-                </div>
-                <button
-                    className="btn btn-primary btn-lg btn-block"
-                    type="submit"
-                >
-                    Proceed to Payment
-                </button>
                 {/* </form> */}
             </div>
         );
@@ -114,67 +90,3 @@ class CheckoutFormCard extends Component {
 }
 
 export default CheckoutFormCard;
-
-{
-    /* <div className="row">
-<div className="col-md-6 mb-3">
-    <label for="cc-name">Name on card</label>
-    <input
-        type="text"
-        className="form-control"
-        id="cc-name"
-        placeholder=""
-        required
-    ></input>
-    <small className="text-muted">
-        Full name as displayed on card
-    </small>
-    <div className="invalid-feedback">
-        Name on card is required
-    </div>
-</div>
-<div className="col-md-6 mb-3">
-    <label for="cc-number">
-        Credit card number
-    </label>
-    <input
-        type="text"
-        className="form-control"
-        id="cc-number"
-        placeholder=""
-        required
-    ></input>
-    <div className="invalid-feedback">
-        Credit card number is required
-    </div>
-</div>
-</div>
-<div className="row">
-<div className="col-md-3 mb-3">
-    <label for="cc-expiration">Expiration</label>
-    <input
-        type="text"
-        className="form-control"
-        id="cc-expiration"
-        placeholder=""
-        required
-    ></input>
-    <div className="invalid-feedback">
-        Expiration date required
-    </div>
-</div>
-<div className="col-md-3 mb-3">
-    <label for="cc-cvv">CVV</label>
-    <input
-        type="text"
-        className="form-control"
-        id="cc-cvv"
-        placeholder=""
-        required
-    ></input>
-    <div className="invalid-feedback">
-        Security code required
-    </div>
-</div>
-</div> */
-}
