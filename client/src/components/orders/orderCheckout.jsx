880cad8abe4140413aa054a0b32db9944832b6c4
Achira Dias <achira828@gmail.com>
2022-06-25 21:03:10 +0530

    Update orderCheckout.jsx

--- client/src/components/orders/orderCheckout.jsx
+++ client/src/components/orders/orderCheckout.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CheckoutCartCard from "./checkoutCartCard";
 import CheckoutFormCard from "./checkoutFormCard";
 import { getCartProducts } from "../../services/cartService";
+import NotFound from "../notFound";
 
 class OrderCheckoutForm extends Component {
     state = {
@@ -13,6 +14,7 @@ class OrderCheckoutForm extends Component {
         const { data: variant } = await getCartProducts(
             localStorage.getItem("cart_id")
         );
+
         let orderTotal = 0;
         if (variant) {
             variant.forEach((element) => {
@@ -28,38 +30,44 @@ class OrderCheckoutForm extends Component {
         await this.CartProducts();
     }
 
-
     render() {
-        const { variant } = this.state;
-        console.log(variant)
-        return (
-            <div className="container h-100 py-5">
+        const { variant, orderTotal } = this.state;
+        console.log(variant, orderTotal);
+        if (variant && orderTotal) {
+            return (
+                <div className="container h-100 py-5">
+                    <div className="row d-flex h-100">
+                        <h3 className="d-inline-block">Checkout</h3>
+                        {/* cart card */}
+                        <div className="col-md-4 order-md-2 mb-4 ">
+                            <h4 className="d-flex justify-content-between align-items-center mb-3">
+                                <span className="text-muted">Your cart</span>
+                                <span className="badge badge-secondary badge-pill">
+                                    {variant.length}
+                                </span>
+                            </h4>
+                            <CheckoutCartCard
+                                cardDetails={variant}
+                                cartTotal={orderTotal}
+                            />
+                        </div>
+                        {/* Form Area */}
+                        <div className="col-md-8 order-md-1">
+                            <h4 className="d-flex justify-content-between align-items-center mb-3">
+                                <span className="text-muted">
+                                    Order Details
+                                </span>
+                            </h4>
 
-                <div className="row d-flex h-100">
-                    <h3 className="d-inline-block">Checkout</h3>
-                    {/* cart card */}
-                    <div className="col-md-4 order-md-2 mb-4 ">
-                        <h4 className="d-flex justify-content-between align-items-center mb-3">
-                            <span className="text-muted">Your cart</span>
-                            <span className="badge badge-secondary badge-pill">
-                                3
-                            </span>
-                        </h4>
-                        <CheckoutCartCard />
-                    </div>
-                    {/* Form Area */}
-                    <div className="col-md-8 order-md-1">
-                        <h4 className="d-flex justify-content-between align-items-center mb-3">
-                            <span className="text-muted">Order Details</span>
-                        </h4>
-                        
-                            <CheckoutFormCard />
-                        
+                            <CheckoutFormCard
+                                cardDetails={variant}
+                                cartTotal={orderTotal}
+                            />
+                        </div>
                     </div>
                 </div>
-
-            </div>
-        );
+            );
+        }
     }
 }
 
