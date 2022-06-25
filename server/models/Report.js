649b8cf8eeea30c63d7af2b48a16c83ead4a4adc
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-06-25 22:25:47 +0530

    DB query for product interest report

--- server/models/Report.js
+++ server/models/Report.js
@@ -5,7 +5,37 @@ dotenv.config();
 
 class Report {
     static async getProductInterest(productId) {
-        return null;
+        const get_product_interest_query = `
+            select
+                year(o.date) year,
+                month(o.date) month,
+                count(*) count
+            from
+                \`order\` o
+                join
+                    cart_product cp
+                    on o.cart_id = cp.cart_id
+                join
+                    variant v
+                    on cp.variant_id = v.variant_id
+                join
+                    product p
+                    on v.product_id = p.product_id
+            where
+                p.product_id = ?
+            group by
+                year(o.date),
+                month(o.date)
+            order by
+                count desc
+        `;
+
+        const [productInterestData, _] = await db.execute(
+            get_product_interest_query,
+            [productId]
+        );
+
+        return productInterestData;
     }
 }
 
