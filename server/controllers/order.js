d80d1ff15b6bb5869d79f03f51e89e73272e86ae
Achira Dias <achira828@gmail.com>
2022-06-26 16:07:30 +0530

    Update order.js

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -7,26 +7,33 @@ class OrderController {
         res.send(allOrders[0]);
     }
 
-    static async getOrderCart(req, res, next){
-        const {id} = req.params;
+    static async getOrderCart(req, res, next) {
+        const { id } = req.params;
         const orderCart = await Order.getOrderCart(id);
         const orderDetails = await Order.getOrderById(id);
-        const orderArray = {orderDetails: orderDetails, orderCart: orderCart[0]};
+        const orderArray = {
+            orderDetails: orderDetails,
+            orderCart: orderCart[0],
+        };
         console.log("api - order id: ", id);
         res.send(orderArray);
     }
 
-    static async setOrderDetails(req, res, next){
-        const test  = req.body;
-        
+    static async setOrderDetails(req, res, next) {
+        const test = req.body;
+        if (OrderController.validateData(test)) {
+            console.log("valid");
+        }
+
         console.log(test);
     }
 
     static validateData(data) {
-        
+        data.data.forEach((element) => {
+            element === null;
+            return false;
+        });
     }
-
-
 }
 
 module.exports.OrderController = OrderController;
