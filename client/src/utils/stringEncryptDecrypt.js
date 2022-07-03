1354a4224830070bd87f04323890d365a902acf0
Achira Dias <achira828@gmail.com>
2022-07-03 16:09:57 +0530

    Create stringEncryptDecrypt.js

---
+++ client/src/utils/stringEncryptDecrypt.js
@@ -0,0 +1,24 @@
+import AES from "crypto-js/aes";
+import ENC from "crypto-js/enc-utf8";
+
+const SECRET_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
+
+export const stringEncrypt = (obj) => {
+    const encrypted = AES.encrypt(JSON.stringify(obj), SECRET_KEY).toString();
+    return encodeURIComponent(encrypted);
+};
+
+export const stringDecrypt = (encryptedText) => {
+    // Here use .toString(ENC) directly, not .toString(ENC.Utf8)
+    const decryptedStr = AES.decrypt(
+        decodeURIComponent(encryptedText),
+        SECRET_KEY
+    ).toString(ENC);
+
+    try {
+        const result = JSON.parse(decryptedStr);
+        return result;
+    } catch (error) {
+        return null;
+    }
+};
