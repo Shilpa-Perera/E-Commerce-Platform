e2a9e04fbdfdb0cf2cd5f41301f89dbd9090a711
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-07-04 18:09:08 +0530

    Send estimated delivery with variant

--- server/controllers/variant.js
+++ server/controllers/variant.js
@@ -1,10 +1,23 @@
 const _ = require("lodash");
+const jwt = require("jsonwebtoken");
 const { Variant, validateVariant } = require("../models/Variant");
+const config = require("config");
+const ROLE = require("../util/roles.json");
+const { CustomerAddress } = require("../models/Customer");
+const { Delivery } = require("../util/delivery");
 
 class VariantController {
     static async getVariant(req, res, next) {
         const { body } = req.query;
         const { product_id, options } = JSON.parse(body);
+
+        const token = req.header("x-auth-token");
+        let user = null;
+        try {
+            user = token
+                ? jwt.verify(token, config.get("jwtPrivateKey"))
+                : null;
+        } catch (e) {}
 
         let variantIds = false;
         let variantIdResults = [];
@@ -25,8 +38,25 @@ class VariantController {
         if (variantIds.length === 1) {
             const variantId = variantIds[0];
             const variant = await Variant.getVariant(variantId);
+
             const allImages = await Variant.fetchAllImages(variantId);
             variant.images = allImages[0];
+
+            if (user && user.role === ROLE.CUSTOMER) {
+                const addresses = await CustomerAddress.getAddressesById(
+                    user.user_id
+                );
+                for (const address of addresses) {
+                    address.estimated_delivery =
+                        await Delivery.calcVariantDeliveryEstimation(
+                            address.postal_code,
+                            variant.quantity
+                        );
+                }
+
+                variant.addresses = addresses;
+            }
+
             return res.send(variant);
         }
         return res.send({ variant_id: 0 });
