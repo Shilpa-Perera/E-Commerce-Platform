213a75f9f7b8be5e3e35aef162f368d1c96e54f8
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-07-05 17:10:24 +0530

    Used common report component in product interest report

--- client/src/components/reports/productInterestReport/productInterestReport.jsx
+++ client/src/components/reports/productInterestReport/productInterestReport.jsx
@@ -1,26 +1,7 @@
-import React, { Component } from "react";
-import {
-    BarElement,
-    CategoryScale,
-    Chart as ChartJS,
-    Legend,
-    LinearScale,
-    Title,
-    Tooltip,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { getChartBackgroundColors } from "../../../utils/chartColors";
+import React from "react";
+import Report from "../../common/report";
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
-
-class ProductInterestReport extends Component {
+class ProductInterestReport extends Report {
     months = [
         "Jan",
         "Feb",
@@ -36,79 +17,10 @@ class ProductInterestReport extends Component {
         "Dec",
     ];
 
-    getOptions = () => {
-        return {
-            indexAxis: "y",
-            elements: {
-                bar: {
-                    borderWidth: 2,
-                },
-            },
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false,
-                },
-                title: {
-                    display: true,
-                    text: "Product Interest Report",
-                },
-            },
-            scale: {
-                ticks: {
-                    precision: 0,
-                },
-            },
-            scales: {
-                x: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: "Orders Count",
-                    },
-                },
-                y: {
-                    display: true,
-                    title: {
-                        display: true,
-                        text: "Month",
-                    },
-                },
-            },
-        };
-    };
-
-    getData = ({ product_title }, reportData) => {
-        const data = {
-            labels: [],
-            datasets: [
-                {
-                    label: product_title,
-                    data: [],
-                    borderColor: "rgb(53, 162, 235)",
-                    backgroundColor: "rgba(53, 162, 235, 0.5)",
-                },
-            ],
-        };
-
-        if (reportData.length > 0) {
-            for (const reportDataItem of reportData) {
-                data.labels.push(
-                    `${this.months[reportDataItem.month - 1]} ${
-                        reportDataItem.year
-                    }`
-                );
-                data.datasets[0].data.push(reportDataItem.count);
-            }
-
-            const { borderColors, bgColors } = getChartBackgroundColors(
-                reportData.length
-            );
-            data.datasets[0].borderColor = borderColors;
-            data.datasets[0].backgroundColor = bgColors;
-        }
-
-        return data;
+    getLabel = (reportDataItem) => {
+        return `${this.months[reportDataItem.month - 1]} ${
+            reportDataItem.year
+        }`;
     };
 
     renderCanvasHeading = (title) => {
@@ -119,16 +31,6 @@ class ProductInterestReport extends Component {
                 </div>
                 <div className="d-flex justify-content-center mb-4">
                     <h3>{title}</h3>
-                </div>
-            </div>
-        );
-    };
-
-    renderCanvas = (options, data) => {
-        return (
-            <div className="container">
-                <div className="d-flex justify-content-center">
-                    <Bar options={options} data={data} />
                 </div>
             </div>
         );
