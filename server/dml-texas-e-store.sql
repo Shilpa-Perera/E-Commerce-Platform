bd9adadd2918c22399e44bad12cb953415d1e4ea
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-07-06 17:12:46 +0530

    Updated categories ddl

--- server/dml-texas-e-store.sql
+++ server/dml-texas-e-store.sql
@@ -2,11 +2,11 @@ use texas_e_store;
 
 # admin
 
-INSERT INTO admin (name, email, password) VALUES  ('Achira', 'achira@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
-INSERT INTO admin (name, email, password) VALUES ('Chaturanga', 'chathuranga@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
-INSERT INTO admin (name, email, password) VALUES ('Sasitha', 'sasitha@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
-INSERT INTO admin (name, email, password) VALUES ('Thamindu', 'thamindu@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
-INSERT INTO admin (name, email, password) VALUES ('Shilpa', 'shilpa@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
+INSERT INTO admin (name, email, password) values  ('Achira', 'achira@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
+INSERT INTO admin (name, email, password) values ('Chaturanga', 'chathuranga@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
+INSERT INTO admin (name, email, password) values ('Sasitha', 'sasitha@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
+INSERT INTO admin (name, email, password) values ('Thamindu', 'thamindu@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
+INSERT INTO admin (name, email, password) values ('Shilpa', 'shilpa@gmail.com', '$2b$10$T98uZ8xm/c7GACggl/w5Re3KwBTwISbpBYYG1p9MyXi1A6njuc9ka');
 
 #categories
 
@@ -26,29 +26,69 @@ insert into category(category_name) values ('Memory Cards');
 insert into category(category_name) values ('Mice');
 insert into category(category_name) values ('Printers');
 
-insert into sub_category(sub_category_name) values ('Apple');
-insert into sub_category(sub_category_name) values ('Nikon');
-insert into sub_category(sub_category_name) values ('SanDisk');
-insert into sub_category(sub_category_name) values ('Transcend');
-insert into sub_category(sub_category_name) values ('Samsung');
-insert into sub_category(sub_category_name) values ('Nokia');
-insert into sub_category(sub_category_name) values ('Hp');
-insert into sub_category(sub_category_name) values ('Asus');
-insert into sub_category(sub_category_name) values ('Acer');
-insert into sub_category(sub_category_name) values ('Huawei');
+insert into sub_category (sub_category_name) values ('Apple');
+insert into sub_category (sub_category_name) values ('Nikon');
+insert into sub_category (sub_category_name) values ('SanDisk');
+insert into sub_category (sub_category_name) values ('Transcend');
+insert into sub_category (sub_category_name) values ('Samsung');
+insert into sub_category (sub_category_name) values ('Nokia');
+insert into sub_category (sub_category_name) values ('Hp');
+insert into sub_category (sub_category_name) values ('Asus');
+insert into sub_category (sub_category_name) values ('Acer');
+insert into sub_category (sub_category_name) values ('Huawei');
+insert into sub_category (sub_category_name) values ('ZTE');
+insert into sub_category (sub_category_name) values ('PROLiNK');
+insert into sub_category (sub_category_name) values ('Wireless');
+insert into sub_category (sub_category_name) values ('4 G');
+insert into sub_category (sub_category_name) values ('ADSL');
+insert into sub_category (sub_category_name) values ('Gaming');
 
-
-insert into category_link (category_id, sub_category_id) values (1,1);
-insert into category_link (category_id, sub_category_id) values (1,5);
-insert into category_link (category_id, sub_category_id) values (1,6);
-insert into category_link (category_id, sub_category_id) values (1,10);
-insert into category_link (category_id, sub_category_id) values (2,1);
-insert into category_link (category_id, sub_category_id) values (2,5);
-insert into category_link (category_id, sub_category_id) values (3,1);
-insert into category_link (category_id, sub_category_id) values (3,8);
-insert into category_link (category_id, sub_category_id) values (3,9);
-insert into category_link (category_id, sub_category_id) values (4,2);
+insert into category_link (category_id, sub_category_id) values (1, 1);
+insert into category_link (category_id, sub_category_id) values (2, 1);
+insert into category_link (category_id, sub_category_id) values (3, 1);
 insert into category_link (category_id, sub_category_id) values (6, 1);
+insert into category_link (category_id, sub_category_id) values (8, 1);
+insert into category_link (category_id, sub_category_id) values (9, 1);
+insert into category_link (category_id, sub_category_id) values (12, 1);
+insert into category_link (category_id, sub_category_id) values (14, 1);
+insert into category_link (category_id, sub_category_id) values (4, 2);
+insert into category_link (category_id, sub_category_id) values (7, 3);
+insert into category_link (category_id, sub_category_id) values (13, 3);
+insert into category_link (category_id, sub_category_id) values (7, 4);
+insert into category_link (category_id, sub_category_id) values (13, 4);
+insert into category_link (category_id, sub_category_id) values (1, 5);
+insert into category_link (category_id, sub_category_id) values (2, 5);
+insert into category_link (category_id, sub_category_id) values (5, 5);
+insert into category_link (category_id, sub_category_id) values (6, 5);
+insert into category_link (category_id, sub_category_id) values (7, 5);
+insert into category_link (category_id, sub_category_id) values (8, 5);
+insert into category_link (category_id, sub_category_id) values (9, 5);
+insert into category_link (category_id, sub_category_id) values (14, 5);
+insert into category_link (category_id, sub_category_id) values (1, 6);
+insert into category_link (category_id, sub_category_id) values (9, 6);
 insert into category_link (category_id, sub_category_id) values (3, 7);
+insert into category_link (category_id, sub_category_id) values (5, 7);
+insert into category_link (category_id, sub_category_id) values (8, 7);
+insert into category_link (category_id, sub_category_id) values (12, 7);
+insert into category_link (category_id, sub_category_id) values (14, 7);
+insert into category_link (category_id, sub_category_id) values (15, 7);
+insert into category_link (category_id, sub_category_id) values (3, 8);
+insert into category_link (category_id, sub_category_id) values (5, 8);
+insert into category_link (category_id, sub_category_id) values (8, 8);
+insert into category_link (category_id, sub_category_id) values (12, 8);
+insert into category_link (category_id, sub_category_id) values (14, 8);
+insert into category_link (category_id, sub_category_id) values (3, 9);
+insert into category_link (category_id, sub_category_id) values (5, 9);
+insert into category_link (category_id, sub_category_id) values (8, 9);
+insert into category_link (category_id, sub_category_id) values (12, 9);
+insert into category_link (category_id, sub_category_id) values (14, 9);
+insert into category_link (category_id, sub_category_id) values (1, 10);
+insert into category_link (category_id, sub_category_id) values (2, 10);
+insert into category_link (category_id, sub_category_id) values (10, 11);
+insert into category_link (category_id, sub_category_id) values (10, 12);
+insert into category_link (category_id, sub_category_id) values (10, 13);
+insert into category_link (category_id, sub_category_id) values (10, 14);
+insert into category_link (category_id, sub_category_id) values (10, 15);
+insert into category_link (category_id, sub_category_id) values (8, 16);
 
 
