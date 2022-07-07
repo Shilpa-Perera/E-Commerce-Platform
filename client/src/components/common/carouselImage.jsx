261a3b9bb4e2bcd9d21767413b998cd18e22a1e2
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-07-07 13:39:24 +0530

    Fixed carousel images not updating with variant

--- client/src/components/common/carouselImage.jsx
+++ client/src/components/common/carouselImage.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function CarouselImage({ img, defaultImg }) {
     const [image, setImage] = useState(`${img}`);
     const [error, setError] = useState(false);
+
+    useEffect(() => {
+        setImage(`${img}`);
+    }, [img])
 
     return (
         <img
