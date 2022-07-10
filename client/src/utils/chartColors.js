76072866aa01b68ad4b1918cf068ae44e85c70c8
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-07-10 07:50:13 +0530

    Updated color matching function for charts

--- client/src/utils/chartColors.js
+++ client/src/utils/chartColors.js
@@ -10,16 +10,8 @@ function getRandomColor() {
 }
 
 function borderColorToBgColor(hex) {
-    let c;
-
-    if (/^#([A-Fa-f\d]{3}){1,2}$/.test(hex)) {
-        c = hex.substring(1).split("");
-
-        if (c.length === 3) {
-            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-
-        c = `0x${c.join("")}`;
+    if (/^#[A-Fa-f\d]{6}$/.test(hex)) {
+        const c = `0x${hex.substring(1)}`;
 
         return `rgba(${[(c >> 16) & 255, (c >> 8) & 255, c & 255].join(
             ", "
