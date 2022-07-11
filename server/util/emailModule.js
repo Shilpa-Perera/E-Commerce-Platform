4c335cdf67ca10b198ae5c94662a3cb5db4736f8
Achira Dias <achira828@gmail.com>
2022-07-11 19:26:49 +0530

    Update emailModule.js

--- server/util/emailModule.js
+++ server/util/emailModule.js
@@ -1,7 +1,47 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const senderEmail = "achira.19@cse.mrt.ac.lk";
 class EmailUtil {
     static getTransporter() {
-        
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: senderEmail,
+                pass: "mimfs123",
+            },
+        });
+        return transporter;
+    }
+
+    static sendEmail(
+        recieverEmail,
+        emailSubject,
+        emailBody,
+        bodyIsHtml = false
+    ) {
+        const mailOptions = bodyIsHtml
+            ? {
+                  from: senderEmail,
+                  to: recieverEmail,
+                  subject: emailSubject,
+                  html: emailBody,
+              }
+            : {
+                  from: senderEmail,
+                  to: recieverEmail,
+                  subject: emailSubject,
+                  text: emailBody,
+              };
+
+        EmailUtil.getTransporter().sendMail(
+            mailOptions,
+            function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Email sent: " + info.response);
+                }
+            }
+        );
     }
 }
 
